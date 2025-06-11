4e64dad3cac34b791d9f63b452cc23a68b73fa90
yonioren <thescrew3692@gmail.com>
2025-06-11 15:21:13 +0300

    Create main.py

---
+++ Python/main.py
@@ -0,0 +1,14 @@
+from flask import Flask, render_template
+from controllers.meal_controller import meal_bp
+from controllers.order_controller import order_bp
+
+app = Flask(__name__)
+app.register_blueprint(meal_bp)
+app.register_blueprint(order_bp)
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8080, debug=True)
