42a080ead9e904b8575e0c9f22be4423cee0ca6e
yonio <asdf@asdf.asdf>
2025-06-12 12:51:37 +0300

    Adjust docker file Add custom port for flask

--- Python/source/main.py
+++ Python/source/main.py
@@ -1,6 +1,6 @@
-from Python.source.ProjectPlanner import create_app
+from ProjectPlanner import create_app
 
 app = create_app()
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, host='0.0.0.0', port=6666)
