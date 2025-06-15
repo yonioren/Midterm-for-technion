e9906fd098d0d241a6bdfe3d2b82e6826f0ef52f
yonioren <asdf@asdf.asdf>
2025-06-15 18:07:19 +0300

    Handle Docker build script Remove waste

---
+++ Linux/build_and_run_docker.sh
@@ -0,0 +1,66 @@
+#!/bin/bash
+
+SCRIPT_DIR="$(dirname "$(readlink -f "${0}")")"
+IMAGE="projectplanner"
+TAG="0.0.1"
+FULL_IMAGE_NAME="${IMAGE}:${TAG}"
+CONTAINER_NAME="demo"
+
+case "$1" in
+  "-f"|"--force")
+    FORCE="true"
+    echo "Using FORCE"
+    ;;
+esac
+
+if [ ! -f "${SCRIPT_DIR}/../Python/Dockerfile" ]
+then
+  echo "Could not find the Docker file"
+  exit 127
+fi
+
+if [ ! -d "${SCRIPT_DIR}/../Python/source" ]
+then
+  echo "Could not file the Python source code"
+  exit 127
+fi
+
+if [ ! -f "${SCRIPT_DIR}/../Python/requirements.txt" ]
+then
+  echo "Could not find the Python dependencies file"
+  exit 127
+fi
+
+if [ "${FORCE}" == "true" ]
+then
+  docker kill "${CONTAINER_NAME}" &>/dev/null
+  docker rm "${CONTAINER_NAME}" &>/dev/null
+  docker rmi "${FULL_IMAGE_NAME}" &>/dev/null
+fi
+
+if [ -n "$(docker images -q "${FULL_IMAGE_NAME}" 2>/dev/null)" ]
+then
+  echo "The docker image already exists. run with --force to recreate"
+else
+  cd "${SCRIPT_DIR}/../Python"
+  docker build -t "${FULL_IMAGE_NAME}" .
+  cd -
+fi
+
+if [ -z "$(docker images -q "${FULL_IMAGE_NAME}" 2>/dev/null)" ]
+then
+  echo "The ${FULL_IMAGE_NAME} image does not exist locally. aborting demo"
+  exit 1
+elif [ "$(docker inspect -f "{{.State.Running}}" "${CONTAINER_NAME}" 2>/dev/null)" == "true" ]
+then
+  echo "The app is already running. run with --force to recreate"
+else
+  docker run -it -d --rm --name "${CONTAINER_NAME}" -p 8666:8666 "${FULL_IMAGE_NAME}"
+fi
+
+if [ "${?}" -ne 0 ]
+then
+  echo "An error occurred while running the app"
+else
+  echo "The app is up. Please surf to http://127.0.0.1:8666 to access the site"
+fi
