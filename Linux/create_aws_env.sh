1f9490c84262932bea25d6030ca04afefec5c7bd
yonioren <asdf@asdf.asdf>
2025-06-15 18:08:17 +0300

    cloudformation yaml + sample commands

---
+++ Linux/create_aws_env.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+#aws cloudformation validate-template \
+ #  --template-body file://network-ha.yaml
+
+aws cloudformation create-stack \
+  --stack-name my-network-stack \
+  --template-body file://network-ha.yaml
+
+aws cloudformation wait stack-create-complete \
+  --stack-name my-network-stack
+
+aws cloudformation describe-stacks \
+  --stack-name my-network-stack \
+  --query "Stacks[0].Outputs"
