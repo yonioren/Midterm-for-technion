2939b3ea42a0cce8a675a3e679dd2c6539791e44
yonioren <thescrew3692@gmail.com>
2025-10-17 12:56:09 +0300

    Update main.tf

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -81,6 +81,7 @@ resource "aws_instance" "app" {
   subnet_id                   = aws_subnet.Jenkins_Subnet.id
   vpc_security_group_ids      = [aws_security_group.jenkins-sg.id]
   associate_public_ip_address = true
+  key_name = "MyKey"
 
   user_data = <<-EOF
               #!/bin/bash
